7e5cc5843fe33572cca1450064ec4cb5a470911b
ydolenko <yurii.dolenko@tain.com>
2023-11-21 09:55:56 +0200

    first commit

---
+++ src/test/java/ua/furniture/FurnitureApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.furniture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurnitureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
